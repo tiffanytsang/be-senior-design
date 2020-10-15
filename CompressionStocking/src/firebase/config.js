ec844eab1ad85356733aa4de566157e065cb19dc
Tiffany Tsang <tiffanytsang@Tiffanys-MacBook-Pro.local>
2020-10-14 21:47:17 -0600

    set up firebase

---
+++ CompressionStocking/src/firebase/config.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+import '@firebase/auth';
+import '@firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyDXcCHic9k-3NukMh1ev-XUE-lZYv1Qwr0',
+    authDomain: 'compressionstocking-90ddf.firebaseapp.com',
+    databaseURL: 'https://compressionstocking-90ddf.firebaseio.com',
+    projectId: 'compressionstocking-90ddf',
+    storageBucket: 'compressionstocking-90ddf.appspot.com',
+    messagingSenderId: '769297201074',
+    appId: '1:769297201074:ios:245d0e279de035a878d6ab',
+};
+
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+export { firebase };
