5e412b0cdce0de9983dd4902c18d2e94f3b0c530
Tiffany Tsang <tktsang@seas.upenn.edu>
2021-04-02 17:48:31 -0400

    update pressure pad reading

--- awsiotcore.py
+++ awsiotcore.py
@@ -1,9 +1,6 @@
-#!/usr/bin/python
-
 import spidev
 import time
 from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTClient
-# import RPi.GPIO as GPIO
 
 #Define Variables
 delay = 0.5
@@ -13,11 +10,6 @@ pad_channel = 0
 spi = spidev.SpiDev()
 spi.open(0, 0)
 spi.max_speed_hz=1000000
-
-def helloworld(self, params, packet):
-    print('Received Message from AWS IoT Core')
-    print("Topic: " + packet.topic)
-    print("Payload: ", (packet.payload))
 
 def readadc(adcnum):
     # read SPI data from the MCP3008, 8 channels in total
@@ -29,54 +21,34 @@ def readadc(adcnum):
 
 myMQTTClient = AWSIoTMQTTClient("BE496ClientID") #random key, if another connection using the same key is opened the previous one is auto closed by AWS IOT
 myMQTTClient.configureEndpoint("a2tud3mf4dwlbh-ats.iot.us-east-1.amazonaws.com", 8883)
-
 myMQTTClient.configureCredentials("/home/pi/AWSIoT/root-ca.pem", "/home/pi/AWSIoT/private.pem.key", "/home/pi/AWSIoT/certificate.pem.crt")
-
 myMQTTClient.configureOfflinePublishQueueing(-1) # Infinite offline Publish queueing
 myMQTTClient.configureDrainingFrequency(2) # Draining: 2 Hz
 myMQTTClient.configureConnectDisconnectTimeout(10) # 10 sec
 myMQTTClient.configureMQTTOperationTimeout(5) # 5 sec
 print ('Initiating Realtime Data Transfer From Raspberry Pi...')
 myMQTTClient.connect()
-# myMQTTClient.subscribe("home/helloworld", 1, helloworld)
-# while True:
-#     time.sleep(.5)
-
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(4,GPIO.IN)
 
 try:
     prev_pad_value=0
     while True:
 
         pad_value = readadc(pad_channel)
-        # print("---------------------------------------")
-        # print("Pressure Pad Value: %d" % pad_value)
-        # time.sleep(delay)
-
 
         #if the last reading was low and this one high, alert us
         if (abs(pad_value-prev_pad_value) > 10):
-            # pad_value = readadc(pad_channel)
             print("---------------------------------------")
             print("Pressure Pad Value: %d" % pad_value)
-            # time.sleep(delay)
-            # print("Under Pressure", prev_input, input)
-            # print("Publishing Message from RPI")
             myMQTTClient.publish(
                 topic="home/helloworld",
                 QoS=1,
-                payload="{'Message': 'Message By RPI - pressure changed loooooool !" + str(pad_value)+ " '}"
+                payload=str(pad_value)
             )
+
         #update previous input
         prev_pad_value = pad_value
         #slight pause
         time.sleep(delay)
 
-
 except KeyboardInterrupt:
     pass
-
-
-
-
